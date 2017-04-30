42023ef2a707a856487a9f5e46d94f0042baeedd
Denis Michailov <kamrad.reg@gmail.com>
2017-05-01 03:55:17 +1000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  courseGoals = [
+    {
+      title: 'Master Angular Styling',
+      isActiveGoal: true
+    },
+    {
+      title: 'Understand Angular Animations',
+      isActiveGoal: false
+    },
+    {
+      title: 'Master Angular Animations',
+      isActiveGoal: false
+    },
+  ];
 }
