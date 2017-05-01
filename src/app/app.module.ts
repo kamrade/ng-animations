526b0004238ee63885e4aa10b13ef0cd10f5444c
Denis Michailov <kamrad.reg@gmail.com>
2017-05-01 22:16:53 +1000

    new components quote and author

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { AuthorComponent } from './author/author.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    AuthorComponent
   ],
   imports: [
     BrowserModule,
